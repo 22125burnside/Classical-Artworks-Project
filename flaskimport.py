7aa881d12cf4e66b124bc9ac9ffe2e2db1867892
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-04-29 09:55:30 +1200

    test website and database by dumping data onto a page

--- flaskimport.py
+++ flaskimport.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
+import sqlite3
 
 
 app = Flask(__name__)
 
 
-@app.route("/home")
+@app.route("/")
 def home():
     return render_template("home.html", title="Home")
+
+
+@app.route('/all_artworks')
+def all_artworks():
+    conn = sqlite3.connect("classical.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Artwork")
+    art = cur.fetchall()
+    conn.close()
+    return render_template("all_art.html", art=art)
 
 
 if __name__ == '__main__':
