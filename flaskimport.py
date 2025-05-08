d12266e1eaf7caf0c601579ca0c0cd9f054f8b9d
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-09 10:27:40 +1200

    Added a locations page and updated home page

--- flaskimport.py
+++ flaskimport.py
@@ -14,10 +14,33 @@ def home():
 def all_artworks():
     conn = sqlite3.connect("classical.db")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM Artwork")
+    cur.execute("""SELECT Artwork.art_name, Artwork.type,
+    FoundLocation.found_location, CurrentLocation.current_location, Person.name,
+    Person.role, Century.century, Century.time_period
+    FROM Artwork
+    JOIN FoundLocation ON Artwork.FL_id=FoundLocation.id
+    JOIN CurrentLocation ON Artwork.CL_id= CurrentLocation.id;
+    JOIN ArtworkPerson ON Artwork.id=ArtworkPerson.aid
+    JOIN Person ON ArtworkPerson.pid=Person.id
+    """)
     art = cur.fetchall()
     conn.close()
     return render_template("all_art.html", art=art)
+
+
+@app.route('/location')
+def location():
+    conn = sqlite3.connect("classical.db")
+    cur = conn.cursor()
+    cur.execute("""SELECT Artwork.art_name, Artwork.type,
+    FoundLocation.found_location, CurrentLocation.current_location
+    FROM Artwork
+    JOIN FoundLocation ON Artwork.FL_id=FoundLocation.id
+    JOIN CurrentLocation ON Artwork.CL_id= CurrentLocation.id;
+    """)
+    art = cur.fetchall()
+    conn.close()
+    return render_template("location.html", art=art)
 
 
 if __name__ == '__main__':
